b123f9deacb75587e1f2b992d38dffcbf116cd54
Arko Biswas <arkobiswas@Arkos-MacBook-Air.local>
2023-07-10 13:56:12 +0530

    Day 1

---
+++ Day_1/Day1_Learning.py
@@ -0,0 +1,17 @@
+print("hello world")
+
+#print function -->
+print("------------------ Print Function -------------------")
+print("hello " + input("What is your name?"))
+
+# Python Variables
+print("------------------ Python Variables -------------------")
+name = input("What is your name?")
+print("Hello " + name)
+
+print("------------------ Length of String -------------------")
+name = input("What is your name?")
+length = len(name)
+print("Hello " + name + ", your name is " + str(length) + " characters long.")
+
+
---
+++ Day_1/Day1_Project.py
@@ -0,0 +1,5 @@
+
+print("------------------ Brand Name Generator -------------------")
+first_name = input("Which is your current City?")
+last_name = input("Which is your favourite animal?")
+print(first_name + " " + last_name)
